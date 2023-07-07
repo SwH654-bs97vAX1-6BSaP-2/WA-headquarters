bef86d8899a7774eed5bb87971431b0d900c1b71
100Roads <93051531+100Roads@users.noreply.github.com>
2023-07-07 12:01:50 +0200

    fix

--- src/main.ts
+++ src/main.ts
@@ -29,22 +29,22 @@ WA.onInit().then(() => {
     WA.room.onEnterLayer("floor1").subscribe(() => {
         WA.room.hideLayer("roof1");
         WA.room.hideLayer("walls-bg-front1");
-        WA.room.hideLayer("signs1");
+        WA.room.hideLayer("sign1");
       });
     WA.room.onLeaveLayer("floor1").subscribe(() => {
         WA.room.showLayer("roof1");
         WA.room.showLayer("walls-bg-front1");
-        WA.room.showLayer("signs1");
+        WA.room.showLayer("sign1");
       });
     WA.room.onEnterLayer("floor2").subscribe(() => {
         WA.room.hideLayer("roof2");
         WA.room.hideLayer("walls-bg-front2");
-        WA.room.hideLayer("signs2");
+        WA.room.hideLayer("sign2");
       });
     WA.room.onLeaveLayer("floor2").subscribe(() => {
         WA.room.showLayer("roof2");
         WA.room.showLayer("walls-bg-front2");
-        WA.room.showLayer("signs2");
+        WA.room.showLayer("sign2");
       });
     WA.room.onEnterLayer("rooms_floor").subscribe(() => {
         WA.room.hideLayer("facade-furniture-bg");
